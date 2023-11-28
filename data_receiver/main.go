d74d2586f91d525ef6ca43e0bf5b98da57e46832
adityash1 <aditya167411@gmail.com>
2023-11-28 22:39:00 +0530

    feat(init): implemented kafka producer

--- data_receiver/main.go
+++ data_receiver/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/adityash1/toll-calculator/types"
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
 )
 
+var kafkaTopic = "obudata"
+
 func main() {
-	recv := NewDataReceiver()
+	recv, err := NewDataReceiver()
+	if err != nil {
+		log.Fatal(err)
+	}
 	http.HandleFunc("/ws", recv.handleWS)
 	log.Fatal(http.ListenAndServe(":30000", nil))
 }
@@ -17,12 +24,50 @@ func main() {
 type DataReceiver struct {
 	msgch chan types.OBUData
 	conn  *websocket.Conn
+	prod  *kafka.Producer
 }
 
-func NewDataReceiver() *DataReceiver {
+func NewDataReceiver() (*DataReceiver, error) {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
+	if err != nil {
+		return nil, err
+	}
+	// Delivery report handler for produced messages
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
 	return &DataReceiver{
 		msgch: make(chan types.OBUData, 128),
+		prod:  p,
+	}, nil
+}
+
+func (dr *DataReceiver) produceData(data *types.OBUData) error {
+	b, err := json.Marshal(data)
+	if err != nil {
+		return err
 	}
+
+	topic := kafkaTopic
+
+	err = dr.prod.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{
+			Topic:     &topic,
+			Partition: kafka.PartitionAny,
+		},
+		Value: b,
+	}, nil)
+
+	return nil
 }
 
 func (dr *DataReceiver) handleWS(w http.ResponseWriter, r *http.Request) {
@@ -48,6 +93,8 @@ func (dr *DataReceiver) wsReceiveLoop() {
 			continue
 		}
 		fmt.Printf("received OBU data from [%d] :: <lat %.2f, long %2f> \n", data.OBUID, data.Lat, data.Long)
-		dr.msgch <- data
+		if err := dr.produceData(&data); err != nil {
+			fmt.Println("kafka produce error:", err)
+		}
 	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/adityash1/toll-calculator
 go 1.21
 
 require (
-	github.com/gorilla/websocket v1.5.1 // indirect
-	golang.org/x/net v0.17.0 // indirect
+	github.com/confluentinc/confluent-kafka-go/v2 v2.3.0
+	github.com/gorilla/websocket v1.5.1
 )
+
+require golang.org/x/net v0.17.0 // indirect
